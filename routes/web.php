4911f48622530d7935915c119f4a0074e913e71a
Saotome <saotome@usp.br>
2022-09-09 00:14:34 -0300

    declara as rotas do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/bookings');
 });
+
+Route::get('/bookings', [BookingController::class, 'index']);
+
+Route::get('/bookings/new', [BookingController::class, 'create']);
+
+Route::post('/bookings', [BookingController::class, 'store']);
+
+Route::delete('/bookings/delete/{id}', [BookingController::class, 'destroy']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
